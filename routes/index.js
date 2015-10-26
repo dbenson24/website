27b303c3d176704adc0f7efef24d8ecd6b5059b0
Derek Benson <derek.benson@tufts.edu>
2015-10-26 19:37:23 +0000

    Imported dbenson24.github.io

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Derek Benson' });
+});
+router.get('/resume', function(req, res, next) {
+  res.render('resume', { title: 'Derek Benson' });
+});
+router.get('/bio', function(req, res, next) {
+  res.render('bio', { title: 'Derek Benson' });
 });
 
 module.exports = router;
